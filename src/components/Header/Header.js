a18b53b607b4b4fbab1b4466555f5c06807f6316
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-04-04 19:07:11 +0600

    Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/home">MuslimShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <CustomLink to="/home">Home</CustomLink>
+                        <CustomLink to="/reviews">Reviews</CustomLink>
+                        <CustomLink to="/dashboard">Dashboard</CustomLink>
+                        <CustomLink to="/blogs">Blogs</CustomLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
